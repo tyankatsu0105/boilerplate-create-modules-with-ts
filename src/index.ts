91bfd79f86ac4d46c672a06d35912a3d84fc6fa7
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-01-05 21:56:34 +0900

    remove files

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { sum } from './utils';
-
-const result = sum(1, 3);
-
-console.log(result);
--- src/utils/__test__/sum.spec.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from '../sum';
-
-describe('sum', () => {
-  it('Will return sum', () => {
-    const result = sum(1, 5);
-
-    expect(result).toBe(6);
-  });
-});
